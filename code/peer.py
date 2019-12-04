6e4718783fe1f09f4fe8bba8f90a8e67f38ca69c
Azad Rahman <azad108rahman@gmail.com>
2019-12-04 18:51:24 -0500

    the peers can talk to each other

--- code/peer.py
+++ code/peer.py
@@ -2,42 +2,137 @@ import os, re, sys, threading, time, json
 from socket import *
 DEBUG = True
 
-myFiles = [] ## List containing all the  completed files that the current 
-			 ## peer has in its shared directory
-
-if len(sys.argv) != 4:
-  print("Usage: {} {} {} {}".format(
-    sys.argv[0], "TRACKER_IP", "TRACKER_PORT", "MIN_ALIVE_TIME"))
-  exit(1)
-
-trackerAddr = sys.argv[1]
-trackerPort = int(sys.argv[2])
-minTime = sys.argv[3]
-
-sharedDir = [os.path.join(root, f) for root, _, files in os.walk('shared/')
-                       for f in files] 
-
-curPeerFile = str(sharedDir[0])
-curPeerFilesize = os.path.getsize(curPeerFile)
-myFiles.append(curPeerFile)
+if __name__ == "__main__":
+	if len(sys.argv) != 4:
+	  print("Usage: {} {} {} {}".format(
+	    "./peer", "TRACKER_IP", "TRACKER_PORT", "MIN_ALIVE_TIME"))
+	  exit(1)
+	myFiles = {} ## Dictionary containing all the files that the current 
+				 ## peer has in its shared directory
+	curPeerData = {}
+	trackerAddr = sys.argv[1]
+	trackerPort = int(sys.argv[2])
+	minAliveTime= sys.argv[3]
 
 
-if DEBUG: print("SIZE OF : " + curPeerFile + " = " + str(curPeerFilesize))
-peerSocket = socket(AF_INET, SOCK_STREAM)
-peerSocket.connect((trackerAddr, trackerPort)) ## TCP connection with the tracker
-## sending it's file info over to the tracker 
-initialPeerData = json.dumps({
-	"filename": curPeerFile,
-	"filesize": curPeerFilesize,
-	"totalFiles": len(sharedDir)
-	})
-peerSocket.send(initialPeerData.encode())
 
-## waiting to be assigned a new ID
-myId = peerSocket.recv(4).decode()
 
-if DEBUG: print("Current Peer's ID = " + str(myId))
 
-while True:
-	sharedDir = [os.path.join(root, f) for root, _, files in os.walk('../shared/')
-                       for f in files]
+
+
+
+
+	# downPort = downSocket.getsockname()[1]
+
+
+def requestFiles(peerSocket, countdown):
+	global trackerAddr, trackerPort, minAliveTime, upSocket, downSocket
+	timedOut = time.time()-countdown >= float(minAliveTime)
+	recvdAll = False
+	while True:
+		if recvdAll: break
+		sharedDir = [os.path.join(root, f) for root, _, files in os.walk('shared/')
+		                       for f in files] 
+		for file in sharedDir:
+			myFiles[sharedDir[0]] = {
+				'id': curPeerData['id'] ,
+				'filesize': int(os.path.getsize(file)),
+				'numchunks': int(os.path.getsize(file)/512)
+			}
+		# peerData = json.dumps(myFiles)
+		# peerSocket.send(peerData.encode())
+		response = peerSocket.recv(1024).decode()
+		
+		filesOffered = json.loads(response)
+		for fname in filesOffered:
+			if fname in sharedDir:
+				chunksProvided = int(filesOffered[fname]['numchunks'])
+				chunksAcquired = int(os.path.getsize(fname)/512)
+
+		for fname in filesOffered:
+			print("FILE OFFERED: "+fname)
+			peerData = {
+				'id': curPeerData['id'],
+				'filename': fname, 
+				'totalFiles': len(sharedDir),
+				'ip': filesOffered[fname]['ip'],
+				'port': filesOffered[fname]['port']
+			}
+			if fname not in sharedDir:
+				if DEBUG: print("NEW PEER FILE CREATED!")
+				open(fname, '+w').close ## just determined fname's existence so make a newfile w fname
+				newDir = [os.path.join(root, f) for root, _, files in os.walk('shared/')
+									for f in files]
+				peerData['filesize'] = os.path.getsize(fname)
+				peerData['numchunks'] = int(os.path.getsize(fname)/512)
+				peerData['totalFiles'] = len(newDir)
+				peerSocket.send(json.dumps(peerData).encode())
+			else:
+				if DEBUG: print("PEER FILE/PARTS OF IT EXISTS!")
+				chunksProvided = int(filesOffered[fname]['numchunks'])
+				chunksAcquired = int(os.path.getsize(fname)/512)
+				if chunksAcquired > chunksProvided:
+					if DEBUG: print("Upload!!")
+					with open(fname, "+rb") as f:
+						print(str( f.read(10)))
+
+						f.seek(chunksProvided*512, 0)
+						## sending 512KB of data over to the peer
+						print (fname + ": " +json.dumps(filesOffered[fname]))
+						upSocket = socket(AF_INET, SOCK_STREAM)
+						upSocket.connect((str(filesOffered[fname]['ip']), int(filesOffered[fname]['port']))) ## TCP connection with the new peer
+						upSocket.send(f.read(524288))
+
+				elif chunksAcquired < chunksProvided:
+					if DEBUG: print("Download!")
+					with open(fname, "+ab") as f:
+						print (fname + ": " +json.dumps(filesOffered[fname]))
+						downSocket = socket(AF_INET, SOCK_STREAM) # create a TCP socket that waits for a new peer to connect
+						downSocket.bind(('', 0))
+						downSocket.listen(8)
+						peerConnectSocket, peerAddr = downSocket.accept()
+						f.write(peerConnectSocket.recv(524288))
+				
+				peerData['filesize'] = os.path.getsize(fname)
+				peerData['numchunks'] = int(os.path.getsize(fname)/512)
+				peerSocket.send(json.dumps(peerData).encode())
+
+
+
+def trackerConnect():
+	sharedDir = [os.path.join(root, f) for root, _, files in os.walk('shared/')
+	                       for f in files] 
+
+	curPeerData['filesize'] = os.path.getsize(sharedDir[0])
+	curPeerData['numchunks'] = int(os.path.getsize(sharedDir[0])/512)
+
+	if DEBUG: print("SIZE OF : " + sharedDir[0] + " = " + str(curPeerData['filesize']))
+	peerSocket = socket(AF_INET, SOCK_STREAM)
+	peerSocket.connect((trackerAddr, trackerPort)) ## TCP connection with the tracker
+	## sending it's file info over to the tracker
+	countdown = time.time() ## start the timer for to check duration that the peer lives
+	initialPeerData = json.dumps({
+		"filename": sharedDir[0],
+		"filesize": os.path.getsize(sharedDir[0]),
+		"totalFiles": len(sharedDir),
+		"port": downPort
+		})
+	peerSocket.send(initialPeerData.encode())
+
+	## waiting to be assigned a new ID
+	myId = peerSocket.recv(4).decode()
+	curPeerData['id'] = myId
+	if DEBUG: print("Current Peer's ID = " + str(myId))
+	for file in sharedDir:
+		myFiles[sharedDir[0]] = {
+			'id': myId,
+			'filesize': int(os.path.getsize(file)),
+			'numchunks': int(os.path.getsize(file)/512)
+		}
+	requestThread = threading.Thread(name="REQUEST THREAD", target=requestFiles, args=(peerSocket, countdown))
+	requestThread.start()
+
+
+if __name__ == "__main__":
+	connectionThread = threading.Thread(name="TRACKER CONNECT", target=trackerConnect)
+	connectionThread.start()
--- code/tracker.py
+++ code/tracker.py
@@ -1,40 +1,54 @@
 import os, sys, threading, time, json
 from socket import *
 
-DEBUG = True
-
-chunkId = 0
-
-class Peer:
-	def __init__(self, id, ip, port, filename, filesize, numchunks, totalChunks):
-		self.id = id
-		self.ip = ip
-		self.port = port
-		self.filename = filename
-		self.filesize = filesize
-		self.numchunks = numchunks ## chunks of original file
-		self.totalChunks = totalChunks ## total chunks all all files
-
-	# def __repr__(self):
-	# 	return '{'+str(self.id) + '|' + self.ip + '|' + self.port + '|' + self.filename + '|' \
-	# 		+ self.filesize + '|' + self.numchunks + '|' + self.totalChunks + '|'
-
-peers = []
-
-
 if __name__ == "__main__":
+	DEBUG = True
+	chunkId = 0
+	peers = [] 
 	trackerSocket = socket(AF_INET, SOCK_STREAM) # create a TCP socket that waits for a new peer to connect
 	trackerSocket.bind(('', 0))
-	trackerSocket.listen(1) 
+	trackerSocket.listen(8)
 	trackerPort = trackerSocket.getsockname()[1]
 	with open('port.txt', 'w') as f:
 		f.write(str(trackerPort))
 	if DEBUG: print('TRACKER_PORT=' + str(trackerPort))
 	peerLock = threading.Lock()
+	ioLock = threading.Lock()
 
-# def signalConnectedPeer(index):
-# 	for i in range(index):
+# class Chunk:
+# 	def __init__(self, filename, lastChunk):
+# 		self.filename = filename
+# 		self.lastChunk = lastChunk
+# 		self.peersWithMe = [] ## list of {peerId, IP, Port}
 
+class Peer:
+	def __init__(self, id, ip, port, filename, filesize, numchunks):
+		self.id = id
+		self.ip = ip
+		self.port = port
+		self.filename = filename
+		self.filesize = filesize
+		self.numchunks = int(numchunks) ## chunks of original file 
+		self.files = {filename: {'numchunks': numchunks, 'ip': ip, 'port': port}}
+
+def signalConnectedPeer(peerId, connectionSocket, peerAddr):
+	while True:
+		for peer in peers:
+			peerInfo = json.dumps(peer.files)
+			connectionSocket.send(peerInfo.encode())
+			peerResponse = connectionSocket.recv(1024).decode()
+			peerResponse = json.loads(peerResponse)
+			with peerLock: 
+				peer.files[peerResponse['filename']] = {
+					'numchunks': peerResponse['numchunks'],
+					'ip': peerResponse['ip'],
+					'port': peerResponse['port']
+					} 
+				print ('_________________________________________')
+				for f in peer.files:
+					print (f+": "+str(peer.files[f]['numchunks']))
+				# print(str(peer.id)+json.dumps(peer.files))
+	connectionSocket.close()
 
 def peerConnect():
 	peerId = 100
@@ -43,18 +57,17 @@ def peerConnect():
 		initialPeerData = connectionSocket.recv(1024).decode()
 		initialPeerData = json.loads(initialPeerData)
 		peerFileSize = initialPeerData.get('filesize')
-		newPeer = Peer(peerId, peerAddr[0], peerAddr[0], initialPeerData.get('filename'), peerFileSize, \
-			peerFileSize/512, peerFileSize/512)
-		print (json.dumps(newPeer.__dict__))
-		print('PEER '+str(peerId)+' CONNECT: OFFERS '+ str(initialPeerData.get('totalFiles')))
+		newPeer = Peer(peerId, peerAddr[0], initialPeerData['port'], initialPeerData['filename'], peerFileSize, \
+			int(peerFileSize/512)) 
+		with ioLock: print('PEER '+str(peerId)+' CONNECT: OFFERS '+ str(initialPeerData['totalFiles']))
 		connectionSocket.send(bytes(str(peerId).encode()))
 		with peerLock:
 			peers.append(newPeer)
-
+		
+		signalConnectedPeerThread = threading.Thread(name="SIGNAL CONNECTED PEER", target=signalConnectedPeer, args=(peerId, connectionSocket, peerAddr))
 		peerId += 1 
-		break
-		# signalConnectedPeerThread = threading.Thread(name="SIGNAL CONNECTED PEER", target=signalConnectedPeer, args=(len(peers)-1),)
-		# signalConnectedPeerThread.start()
+		signalConnectedPeerThread.start()
+
 
 if __name__ == "__main__":
 	connectionThread = threading.Thread(name='PEER CONNECT', target=peerConnect)
